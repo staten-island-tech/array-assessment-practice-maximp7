260c1cde0b1dd74f6af7bbe284979f304a578bc6
maximp7 <144476708+maximp7@users.noreply.github.com>
2023-10-22 18:41:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -84,13 +84,46 @@ const titles = [
   "Little Women",
 ];
 
-books.forEach((books) =>
-console.log(books.authorFirst, books.authorLast, books.name)
-);
-books.forEach((books) =>
-console.log(books.publishDate, books.name)
-);
-WP.forEach((WP.authorFirst, WP.authorLast,WP.na) );
+const book = [
+  // ... (the provided array of book objects)
+];
+
+// 1. Array of authors and the book they wrote
+const authorBookList = books.map(book => `${book.authorFirst} ${book.authorLast} wrote ${book.name}`);
+console.log("Authors and their books:");
+console.log(authorBookList);
+
+// 2. Sort books from oldest to most recent
+const sortedByYear = books.sort((a, b) => a.publishDate - b.publishDate);
+console.log("Books sorted from oldest to most recent:");
+console.log(sortedByYear);
+
+// 3. Sort books alphabetically
+const sortedAlphabetically = books.sort((a, b) => a.name.localeCompare(b.name));
+console.log("Books sorted alphabetically:");
+console.log(sortedAlphabetically);
+
+// 4. Find who wrote War and Peace
+const warAndPeaceAuthor = books.find(book => book.name === "War and Peace");
+console.log(`War and Peace was written by ${warAndPeaceAuthor.authorFirst} ${warAndPeaceAuthor.authorLast}`);
+
+// 5. How many books were written before 1900?
+const booksBefore1900 = books.filter(book => book.publishDate < 1900).length;
+console.log(`Number of books written before 1900: ${booksBefore1900}`);
+
+// 6. Was there at least one book published within the last 100 years?
+const currentYear = new Date().getFullYear();
+const hasBookWithinLast100Years = books.some(book => currentYear - book.publishDate <= 100);
+console.log(`At least one book was published within the last 100 years: ${hasBookWithinLast100Years}`);
+
+// 7. Was every book published within the last 100 years?
+const allBooksWithinLast100Years = books.every(book => currentYear - book.publishDate <= 100);
+console.log(`Every book was published within the last 100 years: ${allBooksWithinLast100Years}`);
+
+// 8. Print a list of books that includes the genre historical
+const historicalBooks = books.filter(book => book.genre.includes("historical")).map(book => book.name);
+console.log("Books that include the genre historical:");
+console.log(historicalBooks);
 
 
 //Array of authors and the book they wrote
@@ -109,3 +142,4 @@ WP.forEach((WP.authorFirst, WP.authorLast,WP.na) );
 //was every book published within the last 100 years?
 
 //print a list of books that "includes" the genre historical
+
